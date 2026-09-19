97b4ab6ed37607397abe4ce27a202fead5bd1fe3
MythicOne75 <mike@magnumcs.com>
2026-09-18 22:20:40 -0400

    Command Center: 2026-09-19 02:20 UTC build

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@
  * an offline launch shows the last run rather than an error. It is an EXPORT either way, and
  * the interface already labels it as one.
  */
-const VERSION = 'genghis-4f6087289435';
+const VERSION = 'genghis-c600be7c3dd9';
 const SHELL = `${VERSION}-shell`;
 const DATA = `${VERSION}-data`;
 
